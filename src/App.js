d1b941c7ab2d6810b8aa115db5487a2c91700d1c
Adam Surya <surya.adam24@gmail.com>
2022-01-14 16:28:50 +0700

    test-skill

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
-
+import kotak from './kotak'
 function App() {
+
+  const [long, setLong] = React.useState("")
+  const [color, setColor] = React.useState("")
+  const [condition, setCondition] = React.useState("")
+  const [show, setShow] = React.useState(false)
+
+  const handleSubmit = () => {
+    setShow(true)
+    console.log(long, color, condition, show)
+  }
+  const kotak = () => {
+    let result = "";
+    for (let i = 1; i <= long; i++) {
+      result += <div className="square1"></div>;
+      
+    }
+    return result;
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Masukkan data: </h1>
+      <input type="text" value={long} onChange={(e) => setLong(e.target.value)} className="input"/>
+      <input type="text" value={color} onChange={(e) => setColor(e.target.value)} className="input"/>
+      <input type="text" value={condition} onChange={(e) => setCondition(e.target.value)} className="input"/>
+      <button type="submit" onClick={handleSubmit} >Submit</button>
+      {show == true ?
+        kotak()
+        : 
+        null
+      }
     </div>
   );
 }
---
+++ src/kotak.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+function kotak() {
+    return (
+        <div className="square1">
+            
+        </div>
+    )
+}
+
+export default kotak
